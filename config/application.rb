1e47c7156f567558c88a56b120d9b569777b1b55
jgeggatt <jonathan@adealio.com>
2012-01-28 00:00:12 -0600

    add cm_formtastic to precompile list

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module CritiqueMe
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += %w( prototype.css cm_formtastic.css )
   end
 end
