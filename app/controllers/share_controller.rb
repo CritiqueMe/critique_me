95e1848c25c12235e367ad878e8a06bf44ded519
jgeggatt <jonathan@adealio.com>
2012-02-20 14:31:37 -0600

    fix for last commit

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -52,10 +52,14 @@ class ShareController < ApplicationController
       friend_ids << f if params["friend_#{i}"]
     end
 
-    response = `curl -s -F 'access_token=#{session[:fb_access_token]}' -F 'message=#{qtext}' -F 'link=#{question_url(@question, :cmfb => 1)}' #{"-F 'picture=#{@question.photo.thumb.url}'" if @question.photo.url} -F 'name=Critique Me' -F 'caption=Ask Your Friends Anything!' -F 'description=#{@user.first_name.capitalize} asked "#{qtext}"' -F 'privacy={"value":"CUSTOM", "allow":"#{friend_ids.join(",")}"}' '#{url}'`
+    response = `curl -s -F 'access_token=#{session[:fb_access_token]}' -F 'message=#{qtext}' -F 'link=#{question_url(@question, :cmfb => 1)}' #{"-F 'picture=#{@question.photo.thumb.url}'" if @question.photo.url} -F 'name=Critique Me' -F 'caption=Ask Your Friends Anything!' -F 'description=#{@user.first_name.capitalize} asked "#{qtext}"' -F 'privacy={"value":"SOME_FRIENDS", "allow":"#{friend_ids.join(",")}"}' '#{url}'`
     Rails.logger.info "aaaa #{response}"
     # TODO: track this posting!
-    render :partial => "share/thanks", :locals => {:num_shared => params['imported_friends'].split(",").length}
+    if friend_ids.length >= 5
+      render :partial => "share/thanks", :locals => {:num_shared => friend_ids.length}
+    else
+      render :partial => "share/oops"
+    end
   end
 
   private
