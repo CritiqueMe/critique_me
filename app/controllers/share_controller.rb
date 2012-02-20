6c222ed037adb6d1f35c2a8a7900e511a2d59ecc
jgeggatt <jonathan@adealio.com>
2012-02-20 14:39:10 -0600

    another fix

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -52,7 +52,7 @@ class ShareController < ApplicationController
       friend_ids << f if params["friend_#{i}"]
     end
 
-    response = `curl -s -F 'access_token=#{session[:fb_access_token]}' -F 'message=#{qtext}' -F 'link=#{question_url(@question, :cmfb => 1)}' #{"-F 'picture=#{@question.photo.thumb.url}'" if @question.photo.url} -F 'name=Critique Me' -F 'caption=Ask Your Friends Anything!' -F 'description=#{@user.first_name.capitalize} asked "#{qtext}"' -F 'privacy={"value":"SOME_FRIENDS", "allow":"#{friend_ids.join(",")}"}' '#{url}'`
+    response = `curl -s -F 'access_token=#{session[:fb_access_token]}' -F 'message=#{qtext}' -F 'link=#{question_url(@question, :cmfb => 1)}' #{"-F 'picture=#{@question.photo.thumb.url}'" if @question.photo.url} -F 'name=Critique Me' -F 'caption=Ask Your Friends Anything!' -F 'description=#{@user.first_name.capitalize} asked "#{qtext}"' -F 'privacy={"value":"CUSTOM", "friends":"SOME_FRIENDS", "allow":"#{friend_ids.join(",")}"}' '#{url}'`
     Rails.logger.info "aaaa #{response}"
     # TODO: track this posting!
     if friend_ids.length >= 5
