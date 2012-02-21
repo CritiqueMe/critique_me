615ce178ff6cab5b3887e6c2717124acf352a991
jgeggatt <jonathan@adealio.com>
2012-02-21 15:43:02 -0600

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :answers
 
   def self.sb_combine_accounts(u1, u2)
-    u2.questions.update_all(:user_id, u1.id)
-    u2.answers.update_all(:user_id, u1.id)
+    u2.questions.update_all(:user_id => u1.id)
+    u2.answers.update_all(:user_id => u1.id)
   end
 end
