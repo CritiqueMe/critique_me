aeba42a47653178fd4a9a7357a21cdc3489f8e6d
jgeggatt <jonathan@adealio.com>
2012-03-19 23:45:19 -0500

    update calls for fb share build_ methods

--- app/models/fb_share_template.rb
+++ app/models/fb_share_template.rb
@@ -1,7 +1,7 @@
 Rails::Engine.mixin __FILE__
 class FbShareTemplate < ActiveRecord::Base
-  def build_message(tracking_object)
-    subs(message, tracking_object)
+  def build_message(tracking_object, sharer)
+    subs(message, tracking_object, sharer)
   end
 
   def build_link(tracking_object, timestamp)
@@ -9,25 +9,25 @@ class FbShareTemplate < ActiveRecord::Base
     "http://#{SEED_BLOCKS_ENGINE_CONFIG[:host]}/question/#{tracking_object.id}?cmfb=#{timestamp}"
   end
 
-  def build_link_display(tracking_object)
-    subs(link_display, tracking_object)
+  def build_link_display(tracking_object, sharer)
+    subs(link_display, tracking_object, sharer)
   end
 
   def build_image(tracking_object)
     tracking_object.photo.try(:thumb).try(:url)
   end
 
-  def build_caption(tracking_object)
-    subs(caption, tracking_object)
+  def build_caption(tracking_object, sharer)
+    subs(caption, tracking_object, sharer)
   end
 
-  def build_description(tracking_object)
-    subs(description, tracking_object)
+  def build_description(tracking_object, sharer)
+    subs(description, tracking_object, sharer)
   end
 
   private
 
-  def subs(str, question)
+  def subs(str, question, inviter)
     str.gsub! "*question_text*", question.question_text_pretty
     str.gsub! "*inviter_first*", inviter.first_name
     str.gsub! "*inviter_last*", inviter.last_name
