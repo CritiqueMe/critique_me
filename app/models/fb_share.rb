2cb6a0ff38ed442adee5573a3f2536e1c2c98e5c
jgeggatt <jonathan@adealio.com>
2012-06-18 11:34:54 -0500

    sb bump

--- app/models/fb_share.rb
+++ app/models/fb_share.rb
@@ -1,6 +1,6 @@
 Rails::Engine.mixin __FILE__
 class FbShare
-  def get_template_for_tracking_object(question, share_type)
+  def self.get_template_for_tracking_object(question, share_type)
     qtype = question.canned_question_id ? 'canned' : (question.public? ? 'public' : 'private')
     FbShareTemplate.active.where(:question_type => qtype, :share_type => share_type).random.first
   end
