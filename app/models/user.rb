51db01a727242db3d5e83523084c61af6c69d9bc
jgeggatt <jonathan@adealio.com>
2012-06-18 14:49:55 -0500

    lock out people who shouldnt be allowed to view a question

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
     u2.questions.update_all(:user_id => u1.id)
     u2.answers.update_all(:user_id => u1.id)
   end
+
+  # TODO: make this less painful...
+  def invited_to_question?(q)
+    inv_count = Invite.where(:invitee_id => self.id, :tracking_object_id => q.id).count > 0
+    inv_count2 = self.email && Invite.where(:invitee_email => self.email, :tracking_object_id => q.id).count > 0
+    fbs_count = FbShare.where(:invitee_id => self.id, :tracking_object_id => q.id).count > 0
+    fbs_count2 = self.fb_user_id && FbShare.where(:invitee_id => self.fb_user_id, :tracking_object_id => q.id).count > 0
+    inv_count || inv_count2 || fbs_count || fbs_count2
+  end
 end
