07ef32d922131e065ded473641ab0deb4c71b346
jgeggatt <jonathan@adealio.com>
2012-06-20 17:25:30 -0500

    adding delayed_jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -90,6 +90,22 @@ ActiveRecord::Schema.define(:version => 20120618200413) do
     t.boolean  "featured",         :default => false
   end
 
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
   create_table "email_from_lines", :force => true do |t|
     t.string   "name"
     t.string   "email_type"
