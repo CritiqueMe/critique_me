99a63dfa395d972d188fbb089899bf48fcbc26e5
jgeggatt <jonathan@adealio.com>
2012-06-22 14:33:04 -0500

    fix for hipchat notifier, sb bump

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -3,8 +3,14 @@ if current_role == "solo" || current_role == "app_master"
   this_app = "critique_me"
 
   branch = node['applications'][this_app]["branch"]
-  rev = revision.strip
+  branch = "<a href='https://github.com/CritiqueMe/critique_me/tree/#{branch}'>#{branch}</a>"
+  rev = "<a href='https://github.com/CritiqueMe/critique_me/commit/#{revision.strip}'>#{revision.strip[0,7]}</a>"
   env = node['environment']['name']
+  env = case env
+        when "staging" then "<a href='http://critiquestg.com'>#{env}</a>"
+        when "production" then "<a href='http://critiqueme.com'>#{env}</a>"
+        else env
+      end
   migrations = node['applications'][this_app]["run_migrations"]
   deploy_str  = "Deployed #{branch} (#{rev}) to #{env}#{" (running migrations)" if migrations == 'true'}."
 
