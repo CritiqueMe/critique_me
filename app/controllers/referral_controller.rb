fa993934f657d47736d23b7b6e2a69f99052aa35
jgeggatt <jonathan@adealio.com>
2012-06-25 20:47:20 -0500

    fix for sending twitter traffic to correct question

--- app/controllers/referral_controller.rb
+++ app/controllers/referral_controller.rb
@@ -2,8 +2,16 @@ Rails::Engine.mixin __FILE__
 class ReferralController < ApplicationController
   private
 
-  def get_tracking_object
-    session[:tracking_object_id] = params[:tracking_object_id]
-    session[:clicked_question_id] = params[:tracking_object_id]
+  def create_twitter_entrance
+    if(share = TwitterShare.find(params[:token]) rescue false)
+      tracker = TwitterEntrance.create(
+          :twitter_share_template_id => share.twitter_share_template_id,
+          :inviter_id => share.inviter_id,
+          :tracking_object_id => share.tracking_object_id
+      )
+      session[:twitter_entrance_id] = tracker.id
+      share.clicked!
+      session[:clicked_question_id] = share.tracking_question_id
+    end
   end
 end
