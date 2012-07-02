4fd332177c6c7145ac977249487dc20c44a79eb5
jgeggatt <jonathan@adealio.com>
2012-07-02 14:11:03 -0500

    sb bump, run hourly reports every 10 minutes for now

--- config/cron_mapreducer.rb
+++ config/cron_mapreducer.rb
@@ -1,6 +1,7 @@
 appname = "critique_me"
 job_type :lockrun_rake, "/usr/bin/lockrun --lockfile=/tmp/:task.lockrun -- sh -c \"cd /data/#{appname}/current && RAILS_ENV=:environment bundle exec rake :task --silent :output\""
 
-every :hour do
+# TODO: change these back to running every hour when the dataset gets too large
+every 10.minutes do
   lockrun_rake "reports:hourly:all"
 end
