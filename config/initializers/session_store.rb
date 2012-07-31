326784834982b01258a61c36971131b896ba7b16
jgeggatt <jonathan@adealio.com>
2012-07-31 17:07:40 -0500

    OOPS

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 domain = Rails.env.production? ? 'critiqueme.com' : (Rails.env.staging? ? 'critiquestg.com' : 'critiqueme.local')
-FoodieRewards::Application.config.session_store :cookie_store, key: '_critique_me_session', :domain => domain
+CritiqueMe::Application.config.session_store :cookie_store, key: '_critique_me_session', :domain => domain
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
