cf7c91b1798beebd8486a97c38576ef2407b457c
jgeggatt <jonathan@adealio.com>
2012-08-09 17:11:31 -0500

    wrong yahoo creds?

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -2,6 +2,6 @@ require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail, "24514668274.apps.googleusercontent.com", "479blV5OlkOIzF3ISuJ8_JNZ"#, :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"
-  importer :yahoo, "dj0yJmk9enZSN3o5c0s1aUxhJmQ9WVdrOU9XaEZZVXBHTnpJbWNHbzlNVGd5T1RrNU5EYzJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yZg--", "3da1c0e187dd2c0a8bf53dbb9bd5dc0a56844b31"
+  importer :yahoo, "dj0yJmk9dnN6c0ZRUWFXend2JmQ9WVdrOU1FTnhkM040TkRRbWNHbzlNVEk1TURjd05USTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD05ZQ--", "86c1af023b5d937eeb1e28177fb74a91a1514f76"
   importer :hotmail, "00000000480CF256", "G8l-H3FlFheKMAjmxVBVeLlEEmIdhWZB"
 end
