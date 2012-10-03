00c54af57cf284d6a624e75a1e4c7376bc493e09
jgeggatt <jonathan@adealio.com>
2012-10-03 13:57:44 -0500

    sb bump

--- app/controllers/tracking_controller.rb
+++ app/controllers/tracking_controller.rb
@@ -5,4 +5,8 @@ class TrackingController < ApplicationController
   def sb_handle_tracking_object(tracker)
     session[:clicked_question_id] = tracker.tracking_object_id
   end
+
+  def sb_handle_already_signed_in_and_finished_with_path(tracker)
+    redirect_to question_path(tracker.tracking_object_id)
+  end
 end
